54988f48de9f492c8f732bd507507a5467d7dc8d
couga <couga@142.232.130.51>
2017-09-07 14:08:56 -0700

    Added View logic and Data to for students

--- Index.php
+++ Index.php
@@ -11,7 +11,57 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-         echo "Hello Out there";
+            include('Student.php');
+
+            $students = array();
+            
+            $first = new Student();
+            $first->surname = "Doe";
+            $first->first_name = "John";
+            $first->add_email('home','john@doe.com');
+            $first->add_email('work','jdoe@mcdonalds.com');
+            $first->add_grade(65);
+            $first->add_grade(75);
+            $first->add_grade(55);
+            $students['j123'] = $first;
+            
+            $second = new Student();
+            $second->surname = "Einstein";
+            $second->first_name = "Albert";
+            $second->add_email('home','albert@braniacs.com');
+            $second->add_email('work1','a_einstein@bcit.ca');
+            $second->add_email('work2','albert@physics.mit.edu');
+            $second->add_grade(95);
+            $second->add_grade(80);
+            $second->add_grade(50);
+            
+            $second = new Student();
+            $second->surname = "Einstein";
+            $second->first_name = "Albert";
+            $second->add_email('home','albert@braniacs.com');
+            $second->add_email('work1','a_einstein@bcit.ca');
+            $second->add_email('work2','albert@physics.mit.edu');
+            $second->add_grade(95);
+            $second->add_grade(80);
+            $second->add_grade(50);
+            $students['a456'] = $second;
+            
+            $third = new Student();
+            $third->surname = "Bock";
+            $third->first_name = "Clint";
+            $third->add_email('home','home@email.com');
+            $third->add_email('work1','phsa@bc.ca');
+            $third->add_email('work2','clint@bcit.ca');
+            $third->add_grade(95);
+            $third->add_grade(80);
+            $third->add_grade(50);
+            $students['a456'] = $third;
+            
+            ksort($students);
+
+            foreach($students as $student)
+                echo $student->toString();
+
         ?>
     </body>
 </html>
